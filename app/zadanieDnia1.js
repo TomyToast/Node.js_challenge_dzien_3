f90408841702af409de8a311cdd611f5f751ce06
Tomek Kulczyński <kulczynski.t@gmail.com>
2019-03-03 21:39:17 +0100

    finished task zadanieDnia1

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,16 @@
 //Twój kod
+const fs = require('fs');
+const crypto = require('crypto');
+
+process.argv.forEach((val, index) => {
+    if (index > 1) {
+        fs.readFile( val, 'utf8', (err, data) => {
+            if(err === null) {
+                const hash = crypto.createHmac('sha256', data).digest('hex');
+                console.log(hash);
+            } else {
+                console.log("Błąd", err);
+            }
+        })
+    }
+})
